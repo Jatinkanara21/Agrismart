65d2bb5212d658c20381a7d5c53c44191cee9820
Jatinkanara21 <jatinkanara2006@gmail.com>
2026-08-18 16:46:05 +0530

    feat: add farm model

---
+++ lib/models/farm.dart
@@ -0,0 +1,19 @@
+class Farm {
+  final String id;
+  final String name;
+  final String location;
+  final double areaAcres;
+  final String soilType;
+  final int soilMoisture;
+  final double ph;
+
+  const Farm({
+    required this.id,
+    required this.name,
+    required this.location,
+    required this.areaAcres,
+    required this.soilType,
+    required this.soilMoisture,
+    required this.ph,
+  });
+}
