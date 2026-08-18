db8b014047ffd835658a4d1c132c033d5461e776
Jatinkanara21 <jatinkanara2006@gmail.com>
2026-08-18 16:45:35 +0530

    feat: modernize AgriSmart app shell

--- lib/app.dart
+++ lib/app.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'screens/splash/splash_screen.dart';
 
-class AgriSmartApp extends StatelessWidget {
+class AgriSmartApp extends StatefulWidget {
   const AgriSmartApp({super.key});
+
+  @override
+  State<AgriSmartApp> createState() => _AgriSmartAppState();
+}
+
+class _AgriSmartAppState extends State<AgriSmartApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +25,8 @@ class AgriSmartApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      home: const SplashScreen(),
+      themeMode: _themeMode,
+      home: SplashScreen(onToggleTheme: _toggleTheme),
     );
   }
 }
